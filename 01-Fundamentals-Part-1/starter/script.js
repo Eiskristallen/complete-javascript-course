38f65194406e14652ab4e62c5ae778d2d8d4e85c
Eiskristallen <qhuangb47@gmail.com>
2024-04-08 22:34:34 -0500

    first day of learning js

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,14 @@
+console.log("this is my script file js!!!");
+// const js = "amazing";
+// if (js === "amazing") alert("hello world");
+
+console.log(40 + 8 + 23 - 10);
+console.log("jonas");
+console.log(23);
+const EVN_NUMBER = 1;
+const firstName = "Qingbei";
+
+const country = "China";
+const continent = "aa";
+const population = "120000000";
+console.log(typeof null);
